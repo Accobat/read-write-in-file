d4e23f7a5bd1d2ec6186114dad6cb138e39cb25f
Алексей Подругин <podrugin@hotmail.com>
2020-09-22 18:43:32 +0300

    Добавьте файлы проекта.

---
+++ read-write-in-file/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace read_write_in_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"boot.ini");
+            StreamWriter sw = new StreamWriter("boot-utf7.txt",false,Encoding.UTF7);
+            sw.WriteLine(sr.ReadToEnd());
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
